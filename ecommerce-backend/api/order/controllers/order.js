fea89bc592d47a9cd62ddac790b5ca88f0a71c22
unknown <costa.sergio.86@gmail.com>
2021-04-27 22:14:28 +0100

    Importing math library to the backend

--- ecommerce-backend/api/order/controllers/order.js
+++ ecommerce-backend/api/order/controllers/order.js
@@ -8,23 +8,54 @@ const stripe = require('stripe')('sk_test_WcmZdHen56ef1qnbEV3CDobj00grxjlNZd')
 
 module.exports = {
     setUpStripe: async (ctx) => {
-        let total = 0
-        let cart = []
+        let total = 100
+        let validatedcart = []
+        let receiptCard = []
+
         //Through ctx.request.body
         //We will receive the products and the qty
+        const {cart} = ctx.request.body
+
+        await Promise.all(cart.map(async product => {
+            const validatedProduct = await strapi.services.product.findOne({
+                id: product.id
+            })
+
+            console.log("validatedProduct", validatedProduct)
+            if(validatedProduct) {
+                validatedProduct.qty = product.qty
+
+                validatedcart.push(validatedProduct)
+
+                receiptCard.push({
+                    id: product.id,
+                    qty: product.qty
+                })
+            }
+        }))
+
+        console.log("validatedcart", validatedcart)
 
         //Only the Id of each product
 
         //Use the data from to calculate the price of each product
         //Basically calculate the total that way
 
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: 2000,
-            currency: 'eur',
-            payment_method_types: ['card'],
-            metadata: {cart: JSON.stringify(cart)}
-        });
+        total = strapi.config.functions.cart.cartTotal(validatedcart)
 
-        return paymentIntent
+        console.log("total", total)
+
+        try {
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: total,
+                currency: 'eur',
+                payment_method_types: ['card'],
+                metadata: {cart: JSON.stringify(receiptCard)}
+            });
+    
+            return paymentIntent
+        } catch(err) {
+           return {error: err.raw.message} 
+        }
     }
 };
